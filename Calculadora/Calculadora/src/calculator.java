0c1b1ee8ff90743124e0ddaf7bc36512cc90e324
JohanGil1502 <johan.gil@uptc.edu.co>
2025-02-13 09:48:11 -0500

    feat: add sum fucntion

---
+++ Calculadora/Calculadora/src/calculator.java
@@ -0,0 +1,9 @@
+public class calculator {
+    public int sum(int number1, int number2){
+        return number1 + number2;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
