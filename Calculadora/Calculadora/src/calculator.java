e5da6d1e6b37da22af0ba0de1033598f096c338d
JohanGil1502 <johan.gil@uptc.edu.co>
2025-02-13 09:49:22 -0500

    feat: add substract function

--- Calculadora/Calculadora/src/calculator.java
+++ Calculadora/Calculadora/src/calculator.java
@@ -2,6 +2,10 @@ public class calculator {
     public int sum(int number1, int number2){
         return number1 + number2;
     }
+    
+    public int substract(int number1, int number2){
+        return number1 - number2;
+    }
 
     public static void main(String[] args) {
         
