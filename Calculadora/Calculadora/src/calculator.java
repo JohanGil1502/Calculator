47c2d29a634ffbd9c784237aa33b7a2165886712
JohanGil1502 <johan.gil@uptc.edu.co>
2025-02-18 11:45:11 -0500

    feat: add resiude function

--- Calculadora/Calculadora/src/calculator.java
+++ Calculadora/Calculadora/src/calculator.java
@@ -18,27 +18,32 @@ public class Calculator {
         return number1 / number2;
     }
 
+    public int residue(int number1, int number2) {
+        return number1 % number2;
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
         Scanner scanner = new Scanner(System.in);
         int option = 0;
 
-        while (option != 5) {
+        while (option != 8) {
             System.out.println("Ingrese la operación que desea realizar:" +
                     "\n1. Sumar" +
                     "\n2. Restar" +
                     "\n3. Multiplicar" +
                     "\n4. Dividir" +
-                    "\n5. Salir");
+                    "\n5. Residuo" +
+                    "\n8. Salir");
 
             option = Integer.parseInt(scanner.nextLine());
-            if (option != 5) {
+            if (option != 8) {
                 System.out.println("Ingrese el primer número");
                 int number1 = Integer.parseInt(scanner.nextLine());
                 System.out.println("Ingrese el segundo número");
                 int number2 = Integer.parseInt(scanner.nextLine());
-    
+
                 switch (option) {
                     case 1:
                         System.out.println("El resultado de la suma es: " + calculator.sum(number1, number2));
@@ -47,19 +52,25 @@ public class Calculator {
                         System.out.println("El resultado de la resta es: " + calculator.substract(number1, number2));
                         break;
                     case 3:
-                        System.out.println("El resultado de la multiplicación es: " + calculator.product(number1, number2));
+                        System.out.println(
+                                "El resultado de la multiplicación es: " + calculator.product(number1, number2));
                         break;
                     case 4:
                         try {
-                            System.out.println("El resultado de la división es: " + calculator.divide(number1, number2));
+                            System.out
+                                    .println("El resultado de la división es: " + calculator.divide(number1, number2));
                         } catch (Exception e) {
                             System.out.println("No es posible dividir por 0");
                         }
                         break;
+                    case 5:
+                        System.out.println(
+                                "El resiudo de la división es: " + calculator.residue(number1, number2));
+                        break;
                     default:
                         break;
                 }
-            } else{
+            } else {
                 System.out.println("Hasta pronto!!");
             }
 
